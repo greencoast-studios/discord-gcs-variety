519275a523ffb944c897c9b7dde9a5a3b0ffa295
moonstar-x <christian.lopez99@outlook.com>
2019-03-05 11:19:16 -0500

    Added lmgtfy command.
    Let me Google that for you.

---
+++ src/commands/lmgtfy.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'lmgtfy',
+  description: 'Let me Google that for you. Place a query and the bot will reply with a link to your Google search.',
+  emoji: ":face_palm: ",
+  execute(message, options) {
+    if (options.args[0]) {
+      let query = options.args.join("+");
+      message.reply(`Here's your results: https://google.com/search?q=${query}`);
+    } else {
+      message.reply(`you need to place a query in order to use this command.`);
+    }
+  }
+};
